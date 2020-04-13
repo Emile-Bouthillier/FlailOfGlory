f26749ab63c72099e8fa6b6e8bd6a07db3d27870
Emile Bouthillier <emile.bouthillier@gmail.com>
2020-04-13 16:05:16 -0400

    Weapon Switch System

---
+++ Assets/WeaponSwitch.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    private int selectedWeapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedWeapon = 0;
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0;
+            }
+            else selectedWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount - 1;
+            }
+            else selectedWeapon--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+
+        if (previousWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    // Weapon Switch
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
