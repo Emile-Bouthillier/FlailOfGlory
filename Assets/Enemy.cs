6657607b30d072979c245acd247048a7f20e0405
Emile Bouthillier <emile.bouthillier@gmail.com>
2020-04-29 23:44:05 -0400

    Added collision detection between the Flail and Enemies
    Also added damage.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody2D EnemyBody;
+    private float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        SpriteRenderer followerSprite = EnemyBody.gameObject.GetComponent<SpriteRenderer>();
+        followerSprite.color = Color.red;
+        health -= damage;
+        if (health <= 0)
+        {
+            followerSprite.color = Color.black;
+        }
+        Debug.Log("Take Damage");
+    }
+}
--- Assets/FlailHead.cs
+++ Assets/FlailHead.cs
@@ -2,8 +2,16 @@
 
 public class FlailHead : MonoBehaviour
 {
-    public Rigidbody2D rbmouse;
+    public Rigidbody2D flailHead;
     private Transform playerPosition;
+    private Vector2 flailPosition;
+    private float flailRange;
+    private float flailRangeSquared;
+
+    public float flailSize;
+    public float flailDamage;
+    public LayerMask whatIsEnemies;
+
 
     // Start is called before the first frame update
     void Start()
@@ -13,32 +21,57 @@ public class FlailHead : MonoBehaviour
 
         // Get the Transform of the Player Object
         playerPosition = PlayerObject.GetComponent<Transform>();
+
+        // Set the flail range
+        flailRange = 2f;
+        flailRangeSquared = flailRange * flailRange;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // // // // // // // // //
+        // Flail head position  //
+        // // // // // // // // //
+
         // Get the vector between the Mouse and the Player
         Vector3 playerToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - playerPosition.position;
 
         Vector2 playerToMouse2D = new Vector2(playerToMouse.x, playerToMouse.y);
         Vector2 playerPosition2D = new Vector2(playerPosition.position.x, playerPosition.position.y);
 
-        // Update the Coin position
-        const float mousefollowerDist = 4;
-        if (playerToMouse2D.sqrMagnitude <= mousefollowerDist)
-        {
-            rbmouse.MovePosition(playerPosition2D + playerToMouse2D);
-        }
-        else { rbmouse.MovePosition(playerPosition2D + (playerToMouse2D.normalized * 2)); }
+        SpriteRenderer followerSprite = flailHead.gameObject.GetComponent<SpriteRenderer>();
 
-        // Get the Player Object
-        SpriteRenderer followerSprite = rbmouse.gameObject.GetComponent<SpriteRenderer>();
-        if (playerToMouse2D.sqrMagnitude > mousefollowerDist)
+        // Cap the Flail position and set the color
+        if (playerToMouse2D.sqrMagnitude <= flailRangeSquared)
         {
+            flailPosition = playerPosition2D + playerToMouse2D;
+            followerSprite.color = Color.white;
+        }
+        else 
+        {
+            flailPosition = playerPosition2D + (playerToMouse2D.normalized * flailRange);
             followerSprite.color = Color.red;
         }
-        else { followerSprite.color = Color.white; }
-        
+
+        // Set the flail position
+        flailHead.MovePosition(flailPosition);
+
+
+        // // // // // // // //
+        // Flail head damage //
+        // // // // // // // //
+
+        Collider2D[] enemieToDamage = Physics2D.OverlapCircleAll(flailHead.position, flailSize, whatIsEnemies);
+        for (int i = 0; i < enemieToDamage.Length; i++)
+        {
+            enemieToDamage[i].GetComponent<Enemy>().TakeDamage(flailDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(flailHead.position, flailSize);
     }
 }
