19930bfc6e73a686f20db95490c641b9155d8356
Emile Bouthillier <emile.bouthillier@gmail.com>
2020-05-10 22:19:22 -0400

    Added enemy health bar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public Rigidbody2D EnemyBody;
+    public float MaxHealth = 50f;
     private float enemyHealth;
     private float enemySpeed;
     private Vector2 enemyDirection;
     public Animator animator;
+    public Slider slider;
+    public GameObject HealthBarUI;
 
     private Transform playerPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyHealth = 50f;
+        enemyHealth = MaxHealth;
         enemySpeed = 0.7f;
         playerPosition = GameObject.Find("Player").GetComponent<Transform>();
+        slider.value = calculateHealth();
     }
 
     // Update is called once per frame
@@ -30,6 +35,24 @@ public class Enemy : MonoBehaviour
         animator.SetFloat("Horizontal", enemyDirection.x);
         animator.SetFloat("Vertical", enemyDirection.y);
         animator.SetFloat("Speed", enemySpeed);
+
+        // Manage Health Bar
+        slider.value = calculateHealth();
+
+        if (enemyHealth < MaxHealth)
+        {
+            HealthBarUI.SetActive(true);
+        }
+
+        if (enemyHealth > MaxHealth)
+        {
+            enemyHealth = MaxHealth;
+        }
+
+        if (enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void FixedUpdate()
@@ -41,12 +64,13 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float damage)
     {
         SpriteRenderer followerSprite = EnemyBody.gameObject.GetComponent<SpriteRenderer>();
-        followerSprite.color = Color.red;
         enemyHealth -= damage;
-        if (enemyHealth <= 0)
-        {
-            followerSprite.color = Color.black;
-        }
         Debug.Log("Take Damage");
+    }
+
+    float calculateHealth()
+    {
+        float health = enemyHealth / MaxHealth;
+        return health;
     }
 }
