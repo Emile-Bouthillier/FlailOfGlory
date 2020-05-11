08498e863502646346af3f73f5bc2345fe4ac07f
Emile Bouthillier <emile.bouthillier@gmail.com>
2020-05-10 23:33:54 -0400

    Player health bar UI displays current health
    Player takes damage when colliding with enemies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float moveSpeed = 1f;
 
     public Rigidbody2D rb;
     public Animator animator;
 
+    public float MaxHealth = 50f;
+    public float CurrentHealth;
+
+    public Slider healthSlider;
+
     Vector2 movement;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+        healthSlider.value = CalculateHealth();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // Input
+        // Movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // Health
+        healthSlider.value = CalculateHealth();
     }
 
     void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    float CalculateHealth()
+    {
+        float health = CurrentHealth / MaxHealth;
+        return health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D others)
+    {
+        CurrentHealth -= 10;
     }
 }
